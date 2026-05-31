93c9e296b5e8c63add540f352e17be4598331e47
yuma0306 <yuma0306895@gmail.com>
2026-05-31 16:38:11 +0700

    修正

--- src/app/member/loading.tsx
+++ src/app/member/loading.tsx
@@ -1,12 +1,10 @@
 import { Inner } from '@/components/Inner/Inner';
-import { Typography } from '@/components/Typography/Typography';
+import { Spinner } from '@/components/Spinner/Spinner';
 
 export default function Loading() {
 	return (
 		<Inner>
-			<Typography size={3} variant="p" color="dark" weight="bold" align="center">
-				会員情報を読み込み中です。
-			</Typography>
+			<Spinner />
 		</Inner>
 	);
 }
